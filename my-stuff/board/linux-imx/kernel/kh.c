1849bcf529db4bf8b7a8579210847f1de92446ea
Emiliano Maccaferri <inbox@emilianomaccaferri.com>
2024-02-19 14:57:53 +0100

    Update kh.c

--- my-stuff/board/linux-imx/kernel/kh.c
+++ my-stuff/board/linux-imx/kernel/kh.c
@@ -8,6 +8,8 @@ atomic64_t LAST_TIMESTAMP = ATOMIC_INIT(-1);
 atomic64_t LAST_DELTA = ATOMIC_INIT(-1);
 atomic64_t SEED = ATOMIC_INIT(0);
 atomic_t GLOBAL_TIMES = ATOMIC_INIT(0);
+atomic_t SEEDS_INDEX = ATOMIC_INIT(0);
+unsigned long long int timer_seeds[20] = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 }; 
 
 inline unsigned long long int get_tsc(void){
 	unsigned long long int cpu_val = 0;
@@ -20,4 +22,6 @@ EXPORT_SYMBOL(LAST_TIMESTAMP);
 EXPORT_SYMBOL(LAST_DELTA);
 EXPORT_SYMBOL(SEED);
 EXPORT_SYMBOL(GLOBAL_TIMES);
+EXPORT_SYMBOL(SEEDS_INDEX);
+EXPORT_SYMBOL(timer_seeds);
 EXPORT_SYMBOL(get_tsc);
