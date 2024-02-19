4f3ea01122a628842f9b7da8efe16e932c35d7ea
Emiliano Maccaferri <inbox@emilianomaccaferri.com>
2024-02-19 14:57:38 +0100

    Update kh.h

--- my-stuff/board/linux-imx/include/kh.h
+++ my-stuff/board/linux-imx/include/kh.h
@@ -1,4 +1,5 @@
 #include <linux/types.h>
+#include <linux/flex_array.h>
 
 #ifndef KH_H
 #define KH_H
@@ -7,6 +8,9 @@ extern atomic64_t LAST_TIMESTAMP;
 extern atomic64_t LAST_DELTA;
 extern atomic64_t SEED;
 extern atomic_t GLOBAL_TIMES;
+extern atomic_t SEEDS_INDEX;
+extern unsigned long long int timer_seeds[20]; // 80 is the maximum (heuristically) number of times the 'try_timer_kh' gets called during kernel initialization, so we can generate 10 seeds
+    // i put 20 seeds just to be safe lol
 extern unsigned long long int get_tsc(void);
 
 #endif
